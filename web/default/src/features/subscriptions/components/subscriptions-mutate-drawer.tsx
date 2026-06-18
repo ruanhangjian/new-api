695e1a8409c1c73ac93c3aba24230345ca9996f9
CaIon <i@caion.me>
2026-06-18 16:53:57 +0800

    feat(subscription): add descriptions for plan purchase amounts in multiple languages

--- web/default/src/features/subscriptions/components/subscriptions-mutate-drawer.tsx
+++ web/default/src/features/subscriptions/components/subscriptions-mutate-drawer.tsx
@@ -355,6 +355,11 @@ export function SubscriptionsMutateDrawer({
                           }
                         />
                       </FormControl>
+                      <FormDescription>
+                        {t(
+                          'Amount the user pays to purchase this plan; the actual currency depends on the payment gateway.'
+                        )}
+                      </FormDescription>
                       <FormMessage />
                     </FormItem>
                   )}
