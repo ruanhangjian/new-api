c3a569624cea33b3f3a9941a920daec56ebc9461
superdavid <493732517@qq.com>
2026-07-20 19:54:31 +0800

    保留尺寸档位间的用户比例选择

--- web/default/src/features/image-workshop/components/image-size-picker.tsx
+++ web/default/src/features/image-workshop/components/image-size-picker.tsx
@@ -231,9 +231,10 @@ function SizePickerPanel({
                           supported ? `${item} 基准分辨率` : '当前模型不支持'
                         }
                         onClick={() => {
-                          setRatio('1:1')
                           setTier(item)
-                          const nextSize = calculateImageSize(item, '1:1')
+                          const activeRatio =
+                            ratio === 'custom' ? customRatio : ratio
+                          const nextSize = calculateImageSize(item, activeRatio)
                           if (
                             nextSize &&
                             isImageWorkshopSizeSupported(capability, nextSize)
